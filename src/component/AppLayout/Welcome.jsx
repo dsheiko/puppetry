c18d0feba56c525e8eb39bc1eee6065f3eca501e
Dmitry Sheiko <dmitry@crytek.com>
2018-11-08 14:33:40 +0100

    polishing intro

--- src/component/AppLayout/Welcome.jsx
+++ src/component/AppLayout/Welcome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button } from "antd";
 import AbstractComponent from "component/AbstractComponent";
 import { getDemoProjectDirectory } from "service/io";
 
@@ -28,31 +29,38 @@ export class Welcome extends AbstractComponent {
     return (
       <div className="welcome">
         <h1>Welcome!</h1>
-        <p>You are using Puppetry, a tool to build End-To-End automation tests without coding.</p>
+        <p>You are using Puppetry, web testing solution for non-developers.</p>
 
         <p>What you can do now is to open a
-          { " " } <a href="#empty" onClick={ this.onOpenDemoProject }>demo project</a> or
-          { " " } <a href="#empty" onClick={ this.onCreateProject }>create a new one</a>.</p>
+          { " " } <Button onClick={ this.onOpenDemoProject }>demo project</Button> or
+          { " " } <Button onClick={ this.onCreateProject }>create a new one</Button>.</p>
+        <br />
+        <p>
+        E2E testing for the Web in a nutshell is about locating a target, applying a browser method on it,
+asserting the new page (DOM) state. Where target can be either a <a onClick={ this.onExtClick }
+            href="https://en.wikipedia.org/wiki/HTML_element">HTML element</a> { " " }
+            or the entire page. Page methods can be such as
+            &quot;goto to a URL&quot;, &quot;make a screenshot&quot;.
+For an element - <code>click</code>, <code>focus</code>, <code>type a text</code>
+and so on. As for assertions we can check for example that element{"'"}s property
+or attribute has a specified value, or element{"'"}s position and size match the provided criteria.
+        </p>
 
-        <p>E2E testing for the Web in a nutshell is about locating a target, applying a command
-        on it, asserting the result complies the given constraints. Where target can be
-        either a <a onClick={ this.onExtClick }
-          href="https://en.wikipedia.org/wiki/HTML_element">HTML element</a> or the entire page.
-        Commands for the page can be such as “goto to a URL”, “make a screenshot”. For an element - click,
-        focus, type a text and so on. As for assertions we can check for example that element’s property
-        or attribute has a specified value, or element’s position
-        and size match the provided criteria.</p>
 
-        <p>Puppetry offers you a UI to create and manage your E2E tests. Namely it allows:</p>
+        <p>Puppetry offers you an easy-to-use UI where you choose browser methods
+        and assertions from a predefined list, with predefined settings, guided by extensive tips.
+        Namely you can do the following:</p>
         <ul>
-          <li>to declare element targets as pairs a arbitrary variable / CSS selector or Xpath.
-Element can be easily located with Chrome DevTool.
-          <a onClick={ this.onExtClick } href="https://www.youtube.com/watch?v=du2Jnm-TzJc">Then you
-just right-click on it and copy selector or XPath</a>.</li>
-          <li>to manage your test structure (suite, test group, test)</li>
-          <li>to manage commands and assertions</li>
+          <li>
+          to declare element targets as pairs <code>variable = locator</code>, where locator can be either
+          CSS selector or Xpath. When using Chrome DevTool
+            <a onClick={ this.onExtClick } href="https://www.youtube.com/watch?v=du2Jnm-TzJc">you
+          just right-click on a target element and copy selector or XPath</a>
+          </li>
+          <li>to manage your test structure ( project, suite, test group, test )</li>
+          <li>to manage browser methods and assertions</li>
         </ul>
-
+        <br />
         <p>If you experience any problems with application, please, report to { " " }
           <a onClick={ this.onExtClick }
             href="https://github.com/dsheiko/puppetry/issues"
