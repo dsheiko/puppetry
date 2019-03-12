b919abd1752edcebc6036c914753e9a679b2a27c
Joel Herold <joel.herold@area23hc.com>
2019-03-11 23:37:35 -0400

    fix @babel/plugin-proposal-decorators build error

--- webpack.config.js
+++ webpack.config.js
@@ -59,11 +59,11 @@ module.exports = {
             options: {
               presets: ["@babel/preset-env", "@babel/preset-react"],
               plugins: [
+                ["@babel/plugin-proposal-decorators", { "legacy": true }],
                 "@babel/plugin-proposal-class-properties",
                 "@babel/plugin-proposal-object-rest-spread",
                 "@babel/plugin-syntax-dynamic-import",
-                "@babel/plugin-transform-runtime",
-                ["@babel/plugin-proposal-decorators", { "legacy": true }]
+                "@babel/plugin-transform-runtime"
               ]
             }
           }]
