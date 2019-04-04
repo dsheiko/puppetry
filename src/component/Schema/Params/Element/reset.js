0cceb6fd0c5ec7b9a8439bb453f8719cfd9e7257
Dmitry Sheiko <dmitry@crytek.com>
2019-04-04 16:56:51 +0200

    adding reset for inputs

---
+++ src/component/Schema/Params/Element/reset.js
@@ -0,0 +1,8 @@
+import { justify } from "service/assert";
+export const reset = {
+  template: ({ targetSeletor }) => justify(
+    `// Reset the input\n`
+  + `await bs.page.$eval( '${ targetSeletor }', el => { el.value = ""; })`),
+  description: `Resets the input element`,
+  params: []
+};
