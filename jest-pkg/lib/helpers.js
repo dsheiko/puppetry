1d91c376a1903d87f6081733e1a1e326ef25d7bd
Dmitry Sheiko <dmitry@crytek.com>
2019-04-23 12:11:07 +0200

    issues 20, 21, setTimeout managable value

--- jest-pkg/lib/helpers.js
+++ jest-pkg/lib/helpers.js
@@ -1,20 +1,26 @@
+
 const { join } = require( "path" ),
-      PATH_SCREENSHOTS = join( __dirname, "/../", "/screenshots"),
       shell = require( "shelljs" ),
-      { dirname } = require( "path" ),
-      /**
-       * @see https://pptr.dev/#?product=Puppeteer&version=v1.5.0&show=api-class-page
-       * @param {string} filePath
-       * @param {Object} [options]
-       * @returns {Object}
-       */
-      png = ( filePath, options = {} ) => {
+      { dirname } = require( "path" );
+
+let PATH_SCREENSHOTS = join( __dirname, "/../", "/screenshots");
+
+    /**
+     * @see https://pptr.dev/#?product=Puppeteer&version=v1.5.0&show=api-class-page
+     * @param {string} filePath
+     * @param {Object} [options]
+     * @returns {Object}
+     */
+const png = ( filePath, options = {} ) => {
         const normalizedPath = filePath.replace( /[^a-zA-Z\d\-\_\\\/]/g, "-" );
         const path = `${PATH_SCREENSHOTS}/${normalizedPath}.png`;
         shell.mkdir( "-p" , dirname( path ) );
         return { path, ...options };
       };
 
+exports.setPngBasePath = ( path ) => {
+  PATH_SCREENSHOTS = path;
+};
 
 exports.makePng = ( ns ) => ( filePath, options = {} ) => {
   return png( `${ns}/${filePath}`, options );
