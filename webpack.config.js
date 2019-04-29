a3757191410c6bb85d559728992df0605ed61053
Dmitry Sheiko <dmitry@crytek.com>
2019-04-29 11:36:34 +0200

    adding support for private class members

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,7 @@ module.exports = {
                 "@babel/plugin-proposal-class-properties",
                 "@babel/plugin-proposal-object-rest-spread",
                 "@babel/plugin-syntax-dynamic-import",
+                "@babel/plugin-proposal-private-methods",
                 "@babel/plugin-transform-runtime"
               ]
             }
