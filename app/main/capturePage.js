ae204376f3c5b6228e3beadf82d6c1c336ef4c4a
Dmitry Sheiko <dmitry@crytek.com>
2019-06-21 17:02:39 +0200

    dev tool for screenshots - to not conflict with Mac

--- app/main/capturePage.js
+++ app/main/capturePage.js
@@ -10,13 +10,13 @@ const { globalShortcut, ipcRenderer } = require( "electron" ),
  */
 module.exports = function( win ) {
 
-  globalShortcut.register('CommandOrControl+Shift+4', () => {
+  globalShortcut.register('CommandOrControl+Shift+Alt+4', () => {
     win.capturePage( img => {
       const date = new Date(),
             ts = Math.round( date.getTime() / 1000 ),
             filename = `screenshot-${ ts }.png`;
       fs.writeFileSync(
-        join( __dirname, "..", "..", "dist", filename ), img.toPNG() );
+        join( __dirname, "..", "..", "screenshots", filename ), img.toPNG() );
         win.webContents.send( E_RENDERER_INFO, `New screenshot ${ filename } added to ./dist` );
     });
   });
